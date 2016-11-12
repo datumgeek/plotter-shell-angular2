241402c7124d052540ac66f85fc1b2bb7ba4e6a4
Datum Geek <mike.graham@ihsmarkit.com>
2016-11-12 14:22:30 -0700

    fixed up the module imports so that sample views can use the views from the shell

--- src/shell.module.ts
+++ src/shell.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -43,7 +43,7 @@ const routes: Routes = [
   ],
   imports: [
     RouterModule.forChild(routes),
-    BrowserModule,
+    CommonModule,
     FormsModule,
     HttpModule
   ],
