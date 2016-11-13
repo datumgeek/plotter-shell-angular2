e99fc9a4471c497f2f597bd8d09c5c19d9b104e6
Datum Geek <mike.graham@ihsmarkit.com>
2016-11-13 06:52:52 -0700

    tab layout working (except for visibility bug)

--- src/compose/compose.component.ts
+++ src/compose/compose.component.ts
@@ -10,7 +10,22 @@ import { ModuleService } from '../module.service';
       <p>{{ errorMessage }}</p>
     </div>
     `,
-  styles: [``]
+  styles: [`
+    :host {
+      position: relative;
+    }
+
+    :host div {
+      position: absolute;
+      margin: 0;
+      padding: 0;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      overflow: hidden;
+    }
+  `]
 })
 export class ComposeComponent implements OnInit {
 
--- src/layouts/tabLayout/tabLayout.component.ts
+++ src/layouts/tabLayout/tabLayout.component.ts
@@ -6,7 +6,7 @@ import { ITab } from '../ITab';
     template: `
         <p-up-down-splitter 
             [hide-up-content]="!state.layout.title && !state.layout.header" 
-            [hide-down-content]="!state.layout.tabs && state.layout.tabs.length <= 0" 
+            [hide-down-content]="!state.layout.tabs || state.layout.tabs.length <= 0" 
             class="up-down-splitter">
 
             <div up-pane class="up-pane" *ngIf="state.layout.title || state.layout.header">
@@ -16,7 +16,7 @@ import { ITab } from '../ITab';
                 <p-compose
                     *ngIf="state.layout.header"
                     [cmodule]="state.layout.header.cmodule"
-                    [component]="state.layout.component"
+                    [component]="state.layout.header.component"
                     [state]="state.layout.header.state">
                 </p-compose>
             </div>
@@ -24,7 +24,7 @@ import { ITab } from '../ITab';
             <div down-pane class="down-pane" *ngIf="state.layout.tabs && state.layout.tabs.length > 0">
 
                 <div class="btn-group main-tabs" data-toggle="buttons">
-                    <label *ngFor="let tab of state.layout.tabs; let ii = index" class="btn btn-primary {tab === state.layout.activeTab ? 'active' : ''}}">
+                    <label *ngFor="let tab of state.layout.tabs; let ii = index" class="btn btn-primary {{tab === state.layout.activeTab ? 'active' : ''}}">
                         <input type="radio" name="mainTabs" [ngModel]="tab" (click)="state.layout.activeTab = tab" [checked]="state.layout.activeTab === tab">
                         <span>{{tab.title}}</span>
                     </label>
@@ -34,7 +34,7 @@ import { ITab } from '../ITab';
                     <div class="main-host">
                         <p-tab-layout
                             *ngFor="let tab of state.layout.tabs"
-                            [state]="tab.header.state"
+                            [state]="{ layout: tab }"
                             [style.visibility]="tab === state.layout.activeTab ? 'visible' : 'hidden'">
                         </p-tab-layout>
                     </div>
@@ -45,7 +45,60 @@ import { ITab } from '../ITab';
         </p-up-down-splitter>
 
         `,
-    styles: [``]
+    styles: [`
+        :host {
+            display: flex;
+            flex-direction: column;
+            margin: 0;
+            padding: 0;
+            border: 0;
+            flex: 1 0 auto;
+            overflow: hidden;
+        }
+
+        :host .down-pane {
+            display: flex;
+            flex-direction: column;
+            position: absolute;
+            margin: 20px;
+            padding: 0;
+            border: 0;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+        }
+
+        :host .main-tabs {
+            
+        }
+
+        :host .main-body {
+            flex: 1 0 auto;
+            position: relative;
+        }
+
+        :host .main-host {
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+        
+        :host p-tab-layout {
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            margin: 0;
+            padding: 0;
+        }
+    `]
 })
 export class TabLayoutComponent {
     @Input() state: any;
